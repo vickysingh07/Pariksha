dff92f98313d2d78a4ff4622be1e6e8bc4afe1cc
Vicky Singh <vicky1910098@akgec.ac.in>
2023-06-23 17:00:15 +0530

    third commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,10 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { SignupComponent } from './pages/signup/signup.component';
 import { LoginComponent } from './pages/login/login.component'
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -22,7 +26,11 @@ import { LoginComponent } from './pages/login/login.component'
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatButtonModule
+    MatButtonModule,
+    MatInputModule,
+    MatFormFieldModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css']
 })
-export class SignupComponent {
+export class SignupComponent implements OnInit{
+ constructor(private userService:UserService){}
 
+public user = {
+  username: '',
+  password: '',
+  firstName: '',
+  lastName: '',
+  email: '',
+  phone: '',
+};
+
+ ngOnInit(): void {}
+ formSubmit(){
+  alert("submitted"); 
+  this.userService.addUser(this.user).subscribe(
+    (data: any)=>{
+      console.log(data);
+      alert('success');
+
+    },(error: any)=>{
+      console.log(error);
+      alert('something went wrong');
+    }
+  )
+ }
 }
