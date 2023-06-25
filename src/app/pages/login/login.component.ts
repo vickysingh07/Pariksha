903fd41c68ccb239d6524128dc4a952ca0cc6aed
Vicky Singh <vicky1910098@akgec.ac.in>
2023-06-25 16:32:50 +0530

    25--06-23

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { LoginService } from 'src/app/services/login.service';
 
 @Component({
   selector: 'app-login',
@@ -7,4 +9,36 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+     loginData = {
+      username: '',
+      password: '',
+     }
+     constructor(private snack:MatSnackBar, private login:LoginService){}
+     ngOnInit(): void{}
+
+     formSubmit(){
+      console.log("login button clicked!!");
+      if(this.loginData.username.trim()=='' || this.loginData.username==null){
+         this.snack.open("Username is required!!", '',{
+          duration: 3000,
+         });
+         return;
+      }
+
+      if(this.loginData.password.trim()=='' || this.loginData.password==null){
+        this.snack.open("password is required!!", '',{
+         duration: 3000,
+        });
+        return;
+      }
+    this.login.generateToken(this.loginData).subscribe((data:any)=>{
+        console.log("success");
+        console.log(data);
+      }, 
+      (error)=>{
+        console.log("Error!");
+        console.log(error);
+      }
+      );
+    }
 }
